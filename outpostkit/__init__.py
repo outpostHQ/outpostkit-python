0409eafacc046f20f902fe03bc20338a0502ae1b
Ajeya Bhat <ajeya@outpost.run>
2024-02-16 16:05:46 +0530

    version update

--- outpostkit/__init__.py
+++ outpostkit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.34"
+__version__ = "0.0.35"
 from outpostkit.client import Client as Client
 from outpostkit.endpoints import Endpoint as Endpoint
 from outpostkit.endpoints import Endpoints as Endpoints
