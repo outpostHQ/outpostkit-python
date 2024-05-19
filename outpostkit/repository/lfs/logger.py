e4ec34c3fb805f04da5f549a3bcafca96e75f03d
Ajeya Bhat <ajeya@outpost.run>
2024-05-19 13:04:52 +0530

    lfs logger

--- outpostkit/repository/lfs/logger.py
+++ outpostkit/repository/lfs/logger.py
@@ -1,8 +1,10 @@
 import logging
 import os
 
-log_file_path = os.path.expanduser("/var/log/outpost/outpostkit.log")
+log_dir = "/tmp"
+log_file_path = os.path.expanduser(f"{log_dir}/outpostkit.log")
 outpost_folder = os.path.dirname(log_file_path)
+
 if not os.path.exists(outpost_folder):
     # Create the ~/.outpost folder if it doesn't exist
     os.makedirs(outpost_folder)
@@ -11,6 +13,7 @@ if not os.path.exists(outpost_folder):
 def create_lfs_logger(name: str):
     _log = logging.getLogger(name)
     _log.handlers.clear()
+    _log.setLevel(10)
     file_handler = logging.FileHandler(log_file_path)
     file_handler.setFormatter(
         logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
